368ba611a603cca9e311141444e04058699c2c10
DanielSanRamirez <danielsanramirez@hotmail.com>
2020-12-09 12:19:07 -0500

    Insertar estudiante

--- src/main/java/com/clearminds/dra/dtos/Estudiante.java
+++ src/main/java/com/clearminds/dra/dtos/Estudiante.java
@@ -16,7 +16,34 @@ public class Estudiante {
 		this.nombre = nombre;
 		this.apellido = apellido;
 	}
-	
-	
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "Estudiante [id=" + id + ", nombre=" + nombre + ", apellido=" + apellido + "]";
+	}
 
 }
